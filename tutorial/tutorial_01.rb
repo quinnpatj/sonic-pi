399855401c9895290ca820294e544c2a1d64265d
Pat Quinn <quinnpatj@gmail.com>
2026-02-11 21:42:40 -0600

    Convert to live_loop

--- tutorial/tutorial_01.rb
+++ tutorial/tutorial_01.rb
@@ -1,7 +1,14 @@
-# Welcome to Sonic Pi
+# Tutorial amen jungle loop
 
-loop do
-  sample :loop_amen, rate: 1.25
-  sample :ambi_piano, rate: choose([0.5, 0.75, 1, 1.25])
-  sleep sample_duration :loop_amen, rate: 1.25
+sample_name = :loop_amen
+
+with_fx :reverb do
+  in_thread(name: :amen) do
+    live_loop :foo do
+      sample sample_name, rate: 1.25
+      #sample :ambi_piano, rate: choose([0.5, 0.75, 1, 1.25])
+      sample :ambi_piano, rate: choose([0.5, 0.75, 1, 1.25])
+      sleep sample_duration(sample_name, rate: 1.25)
+    end
+  end
 end
